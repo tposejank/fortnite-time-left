90406c0d868b592cd672e86cd98817e9daf4efdc
tposejank <81495861+tposejank@users.noreply.github.com>
2022-03-15 19:32:22 -0500

    Upload files

---
+++ main.js
@@ -0,0 +1,9 @@
+function doCalc() {
+    var day = document.getElementById("day");
+    var month = document.getElementById("month");
+    var year = document.getElementById("year");
+
+    console.log(day);
+    console.log(month);
+    console.log(year);
+}
